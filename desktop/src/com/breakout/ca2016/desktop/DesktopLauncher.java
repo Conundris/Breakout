79e12293dfeea65b77f9eae4b5c54d8e78b1adcc
soviet <soviet@test.ch>
2016-11-15 13:08:03 +0100

    Initial commit

---
+++ desktop/src/com/breakout/ca2016/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.breakout.ca2016.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.breakout.ca2016.Breakout;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Breakout(), config);
+	}
+}
