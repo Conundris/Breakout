1f8186565a3de3690e3fd54c9879d7842539e844
t00191944 <te@gef.com>
2016-11-15 15:51:52 +0000

    ScreenStates Implemented

---
+++ core/src/com/breakout/ca2016/MainMenuScreen.java
@@ -0,0 +1,51 @@
+package com.breakout.ca2016;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by t00191944 on 15/11/2016.
+ */
+
+public class MainMenuScreen implements Screen {
+
+    private Breakout game;
+
+    public MainMenuScreen(Breakout game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/com/breakout/ca2016/ScreenType.java
@@ -0,0 +1,9 @@
+package com.breakout.ca2016;
+
+/**
+ * Created by t00191944 on 15/11/2016.
+ */
+public enum ScreenType {
+    MainGame,
+    MainMenu
+}
